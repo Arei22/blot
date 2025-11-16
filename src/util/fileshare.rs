2a379657b0845812706abd1c9637e7a21e9c1506
arei22 <bababaptistetiti@gmail.com>
2025-11-16 09:44:05 +0100

    refactor: move request content into body

--- src/util/fileshare.rs
+++ src/util/fileshare.rs
@@ -7,11 +7,15 @@ use crate::{client::error::ClientError, util::parse_key};
 pub async fn generate_upload() -> Result<String, ClientError> {
     let doup = parse_key::<String>("DOUP_URL")?;
     let token = parse_key::<String>("TOKEN")?;
-    let request_url = format!("{doup}/generate_upload?token={token}");
+    let request_url = format!("{doup}/generate_upload");
 
     let timeout = Duration::new(5, 0);
     let client = reqwest::ClientBuilder::new().timeout(timeout).build()?;
-    let response = client.post(&request_url).send().await?;
+    let response = client
+        .post(&request_url)
+        .json(&serde_json::json!({ "token": token }))
+        .send()
+        .await?;
 
     if response.status().is_success() {
         return Ok(response.text().await?);
@@ -23,29 +27,45 @@ pub async fn generate_upload() -> Result<String, ClientError> {
 pub async fn get_upload(uuid: String, id: i64) -> Result<(), ClientError> {
     let doup = parse_key::<String>("DOUP_URL")?;
     let token = parse_key::<String>("TOKEN")?;
-    let request_url = format!("{doup}/give_upload?token={token}&uuid={uuid}");
+    let request_url = format!("{doup}/give_upload");
 
     sleep(Duration::from_secs(10)).await;
 
     let timeout = Duration::new(5, 0);
     let client = reqwest::ClientBuilder::new().timeout(timeout).build()?;
-    let mut response = client.post(&request_url).send().await?;
+    let mut response = client
+        .post(&request_url)
+        .json(&serde_json::json!({ "token": token, "uuid" : uuid }))
+        .send()
+        .await?;
 
     let mut i = 0;
     while response.status().as_u16() == 202 && i < 2 {
         sleep(Duration::from_secs(10)).await;
-        response = client.post(&request_url).send().await?;
+        response = client
+            .post(&request_url)
+            .json(&serde_json::json!({ "token": token, "uuid" : uuid }))
+            .send()
+            .await?;
         i += 1;
     }
 
     if response.status().as_u16() == 202 {
         sleep(Duration::from_secs(30)).await;
-        response = client.post(&request_url).send().await?;
+        response = client
+            .post(&request_url)
+            .json(&serde_json::json!({ "token": token, "uuid" : uuid }))
+            .send()
+            .await?;
     }
 
     while response.status().as_u16() == 202 {
         sleep(Duration::from_secs(60)).await;
-        response = client.post(&request_url).send().await?;
+        response = client
+            .post(&request_url)
+            .json(&serde_json::json!({ "token": token, "uuid" : uuid }))
+            .send()
+            .await?;
     }
 
     if response.status().is_success() {
